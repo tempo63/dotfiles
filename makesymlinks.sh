b50297e4a4f80d91e94a0a7da9f8348880d83f49
tempo63 <seresbalu@gmail.com>
2021-02-26 01:52:43 -0800

    fixed syntax errors in script

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -1,14 +1,19 @@
-dir = ~/dotfiles
-olddir= ~/dotfiles_old
-file = "bash_profile config tmux.conf bashrc inputrc vimrc"
-#create dotifle_old in homedir
+#!/bin/bash
+######################
+#.make.sh
+######################
 
+#Variables
+dir=~/dotfiles
+olddir=~/dotfiles_old
+file="bash_profile config tmux.conf bashrc inputrc vimrc"
+
+#create dotifle_old in homedir
 echo "Creating $olddir for backup of any existing dotfiles in ~"
 mkdir -p $olddir
 echo "...done"
 
 #change to the dotfiles directory
-
 echo "Changing to the $dir directory"
 
 cd $dir
@@ -16,7 +21,6 @@ echo "...done"
 
 #move any existing dotfiles in homedir to dotfiles_old
 #create symlinks for $files
-
 for file in $files; done
     echo "Moving any existing dotfiles from ~ to $olddir"
     mv ~/.$file ~/dotfiles_old/
